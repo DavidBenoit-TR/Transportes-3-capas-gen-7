aa4274ee976b5c1f74c7d93a49e6126934048ea2
David <mdavid.benoit@gmail.com>
2024-06-25 13:30:06 -0600

    Add - VO, DAL Y BLL de Rutas

---
+++ DAL/DAL_Rutas.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VO;
+
+namespace DAL
+{
+    public class DAL_Rutas
+    {
+        //Create (crear/insertar)
+        public static string InsertarRuta(Rutas_VO ruta)
+        {
+            string salida = "";
+            int respuesta = 0;
+
+            try
+            {
+                respuesta = metodos_Datos.execute_NonQuery("sp_Rutas_Ins",
+                        "@camionId", ruta.CamionId,
+                        "@distancia", ruta.Distancia,
+                        "@fechaSalida", ruta.FechaSalida,
+                        "@fechaLlegadaEstimada", ruta.FechaLlegadaEstima,
+                        "@fechaLlegadaReal", ruta.FechaLlegadaReal,
+                        "@choferid", ruta.ChoferId,
+                        "@direccionOrigenId", ruta.DireccionOrigenId,
+                        "@direccionDestinoId", ruta.DireccionDestinoId
+                    );
+                if (respuesta != 0)
+                {
+                    salida = "Ruta registrada con éxito";
+                }
+                else
+                {
+                    salida = "Ha ocurrido un error";
+                }
+            }
+            catch (Exception e)
+            {
+                salida = "Error: " + e.Message;
+            }
+            return salida;
+        }
+
+        //Read (consultar/obtener)
+        public static List<Rutas_VO> GetRutas(params object[] parametros)
+        {
+            List<Rutas_VO> list_rutas = new List<Rutas_VO>();
+            try
+            {
+                DataSet ds_camiones = metodos_Datos.execute_DataSet("sp_Rutas_Sel", parametros);
+                foreach (DataRow dr in ds_camiones.Tables[0].Rows)
+                {
+                    list_rutas.Add(new Rutas_VO(dr));
+                }
+                return list_rutas;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        //Update (actualizar/modificar)
+        public static string ActualizarRuta(Rutas_VO ruta)
+        {
+            string salida = "";
+            int respuesta = 0;
+
+            try
+            {
+                respuesta = metodos_Datos.execute_NonQuery("sp_Rutas_Upd",
+                        "@camionId", ruta.CamionId,
+                        "@distancia", ruta.Distancia,
+                        "@fechaSalida", ruta.FechaSalida,
+                        "@fechaLlegadaEstimada", ruta.FechaLlegadaEstima,
+                        "@fechaLlegadaReal", ruta.FechaLlegadaReal,
+                        "@choferid", ruta.ChoferId,
+                        "@direccionOrigenId", ruta.DireccionOrigenId,
+                        "@direccionDestinoId", ruta.DireccionDestinoId,
+                        "@idRuta", ruta.IdRuta
+                    ); ;
+                if (respuesta != 0)
+                {
+                    salida = "Ruta Actualizada con éxito";
+                }
+                else
+                {
+                    salida = "Ha ocurrido un error";
+                }
+            }
+            catch (Exception e)
+            {
+                salida = "Error: " + e.Message;
+            }
+            return salida;
+        }
+
+        //Delete (eliminar)
+        public static string EliminarRuta(int id)
+        {
+            string salida = "";
+            int respuesta = 0;
+
+            try
+            {
+                respuesta = metodos_Datos.execute_NonQuery("sp_Rutas_Del",
+                    "@idRuta", id
+                    );
+                if (respuesta != 0)
+                {
+                    salida = "Ruta eliminada con éxito";
+                }
+                else
+                {
+                    salida = "Ha ocurrido un error";
+                }
+            }
+            catch (Exception e)
+            {
+                salida = "Error: " + e.Message;
+            }
+            return salida;
+        }
+
+    }
+}
--- VO/Rutas_VO.cs
+++ VO/Rutas_VO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,50 @@ namespace VO
 {
     public class Rutas_VO
     {
-        //Prueba de Git
+        private int idRuta;
+        private int camionId;
+        private double distancia;
+        private string fechaSalida;
+        private string fechaLlegadaEstima;
+        private string fechaLlegadaReal;
+        private int choferId;
+        private int direccionOrigenId;
+        private int direccionDestinoId;
+
+        public int IdRuta { get => idRuta; set => idRuta = value; }
+        public int CamionId { get => camionId; set => camionId = value; }
+        public double Distancia { get => distancia; set => distancia = value; }
+        public string FechaSalida { get => fechaSalida; set => fechaSalida = value; }
+        public string FechaLlegadaEstima { get => fechaLlegadaEstima; set => fechaLlegadaEstima = value; }
+        public string FechaLlegadaReal { get => fechaLlegadaReal; set => fechaLlegadaReal = value; }
+        public int ChoferId { get => choferId; set => choferId = value; }
+        public int DireccionOrigenId { get => direccionOrigenId; set => direccionOrigenId = value; }
+        public int DireccionDestinoId { get => direccionDestinoId; set => direccionDestinoId = value; }
+
+        public Rutas_VO()
+        {
+            idRuta = 0;
+            camionId = 0;
+            distancia = 0;
+            fechaSalida = "";
+            fechaLlegadaEstima = "";
+            fechaLlegadaReal = "";
+            choferId = 0;
+            direccionOrigenId = 0;
+            direccionDestinoId = 0;
+        }
+
+        public Rutas_VO(DataRow dr)
+        {
+            idRuta = int.Parse(dr["idRuta"].ToString());
+            camionId = int.Parse(dr["camionId"].ToString());
+            distancia = double.Parse(dr["distancia"].ToString());
+            fechaSalida = DateTime.Parse(dr["fechaSalida"].ToString()).ToShortDateString();
+            fechaLlegadaEstima = DateTime.Parse(dr["fechaLlegadaEstima"].ToString()).ToShortDateString();
+            fechaLlegadaReal = DateTime.Parse(dr["fechaLlegadaReal"].ToString()).ToShortDateString();
+            choferId = int.Parse(dr["choferId"].ToString());
+            direccionOrigenId = int.Parse(dr["direccionOrigenId"].ToString());
+            direccionDestinoId = int.Parse(dr["direccionDestinoId"].ToString());
+        }
     }
 }
